673b6d7a2019c53e88feaa3bf628d597b670bff9
jisukuke5 <lingjunjun111@126.com>
2021-08-22 18:49:58 +0800

    格式化JavadocTagTest

--- codes/03/3.1/JavadocTagTest.java
+++ codes/03/3.1/JavadocTagTest.java
@@ -1,6 +1,5 @@
 /**
- * Description: 
- * 网站：<a href="http://www.crazyit.org">疯狂Java联盟</a><br>
+ * Description: 网站：<a href="http://www.crazyit.org">疯狂Java联盟</a><br>
  * Copyright (C), 2001-2020, Yeeku.H.Lee<br>
  * This program is protected by copyright laws. <br>
  * Program Name: <br>
@@ -12,10 +11,11 @@
 public class JavadocTagTest {
     /**
      * 一个得到打招呼字符串的方法
+     * 
      * @param name 该参数指定向谁打招呼
      * @return 返回打招呼的字符串
      */
-    public String hello(String name){
+    public String hello(String name) {
         return name + "，你好！";
     }
 }
